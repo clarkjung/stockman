326f2b7d02b5380086f96391f0fd1bd3911d35db
Jung, Jaeyoon <jaeyoon.jung@sap.com>
2017-01-29 21:06:01 -0800

    investor buy complete

--- investor.py
+++ investor.py
@@ -1,14 +1,19 @@
+from order import Order
+import datetime
+
 class Investor(object):
 
 	def __init__(self, name, genkinzandaka):
 		self.name = name
 		self.genkinzandaka = 10000.00
-		self.stock_list = []
+		self.order_list = []
 
-	def buy(self, stock, number_of_stock):
-		if stock.price * number_of_stock > self.genkinzandaka:
+	def buy(self, stock, konyusu):
+		if stock.price * konyusu > self.genkinzandaka:
 			print "not enough genkinzandaka. Current genkinzandaka: " + self.genkinzandaka + ", required amount of cash: " + stock.price * number_of_stock
 			return None
 
-		self.genkinzandaka = self.genkinzandaka - stock.price * number_of_stock
-		
+		new_order = Order(stock, konyusu, datetime.datetime.now(), stock.price * konyusu)
+		self.order_list.append(new_order)
+		self.genkinzandaka = self.genkinzandaka - stock.price * konyusu
+
