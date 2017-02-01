e3cd8899fd59445a003ef63237f4eed868b67b2d
Jung, Jaeyoon <jaeyoon.jung@sap.com>
2017-02-01 01:35:43 -0800

    find resistance support lines using trendy

---
+++ trendy/main.py
@@ -0,0 +1,24 @@
+# Download Apple price history and save adjusted close prices to numpy array
+#import pandas.io.data as pd
+import pandas_datareader as pdr
+#x = pdr.DataReader("AAPL", "yahoo")['Adj Close']
+x = pdr.get_data_yahoo('AAPL')['Adj Close']
+print x
+
+
+# Make some trendlines
+import trendy
+
+# Generate general support/resistance trendlines and show the chart
+# winow < 1 is considered a fraction of the length of the data set
+trendy.gentrends(x, window = 1.0/3, charts = True)
+
+# Generate a series of support/resistance lines by segmenting the price history
+trendy.segtrends(x, segments = 2, charts = True)  # equivalent to gentrends with window of 1/2
+trendy.segtrends(x, segments = 5, charts = True)  # plots several S/R lines
+
+# Generate smaller support/resistance trendlines to frame price over smaller periods
+trendy.minitrends(x, window = 30, charts = True)
+
+# Iteratively generate trading signals based on maxima/minima in given window
+trendy.iterlines(x, window = 30, charts = True)  # buy at green dots, sell at red dots
